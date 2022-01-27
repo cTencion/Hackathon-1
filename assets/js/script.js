d7083ba2ce6ab02469b80059b93da25beacdb828
Geoffrey BRUNET <brunet.geoffrey@gmail.com>
2022-01-27 12:46:29 +0100

    accordeon faq in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 // MENU BURGER
-const hidden = document.getElementById('hidden')
+const hidden = document.getElementById('hidden');
 const burger_btn = document.getElementById('burger-btn');
 
-burger_btn.addEventListener("click", function(){
+burger_btn.addEventListener("click", function () {
     hidden.classList.toggle("isActive");
 });
 // MENU BURGER
+
+
+
+// ACCORDEON FAQ
+const faccordeon = document.getElementById('faccordeon');
+
+faccordeon.addEventListener("click", function(){
+    this.nodeParent.nextSibling.classList.toggle('showned');
+});
+
+// ACCORDEON FAQ
+
